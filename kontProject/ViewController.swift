1fc6c7533323067640a7488cd842c9c7eb72f525
swatronik <30504255+swatronik@users.noreply.github.com>
2017-10-10 10:28:04 +0300

    v0.2
    coordinate

--- kontProject/ViewController.swift
+++ kontProject/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import Foundation
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    
+    
+    var locationManager:CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
+    
+    
+    func locationManager(_ manager:CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let local=locations[0]
+        print("locations = \(locations)")
+        let kor:MKCoordinateSpan=MKCoordinateSpanMake(local.coordinate.latitude, local.coordinate.longitude)
+        print ("\(kor.latitudeDelta)");
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
-
+    
+    
 }
+
 
