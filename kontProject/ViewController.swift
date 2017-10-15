0532e2ed6a0ce98793d5043d8952b282a6f3cbbd
swatronik <30504255+swatronik@users.noreply.github.com>
2017-10-15 15:21:52 +0300

    v0.6
    realm

--- kontProject/ViewController.swift
+++ kontProject/ViewController.swift
@@ -15,14 +15,29 @@ import SwiftyJSON
 import RealmSwift
 import ObjectMapper
 
+class inform: Object {
+    @objc dynamic var city = ""
+    @objc dynamic var temp = ""
+    @objc dynamic var id = 0
+    @objc dynamic var imageName = ""
+    @objc dynamic var date = NSDate()
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+}
+
 class ViewController: UIViewController,CLLocationManagerDelegate {
     
-    
+    var realm = try! Realm()
     var city:String=""
     var locationManager:CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let results = self.realm.objects(inform.self)
+        if ((results.first != nil)){
+            if((prover(date1: results[results.endIndex-1].date,date2: NSDate()))){
         
         locationManager = CLLocationManager()
         locationManager.delegate = self
@@ -31,20 +46,45 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         let location = locationManager.location
         let local=location?.coordinate
-        //dateFormatter()
         StartWeather(latitude: local!.latitude,longitude: local!.longitude)
+            }else{print("5 minut!!")}
+            
+        }else{ locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        let location = locationManager.location
+        let local=location?.coordinate
+        StartWeather(latitude: local!.latitude,longitude: local!.longitude)}
     }
     
-    func dateFormatter(){
-        let date = NSDate()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .short
-        var dateString = dateFormatter.string(from: date as Date)
-        print(dateString)
-        dateFormatter.dateFormat = "hh mm"
-        dateString = dateFormatter.string(from: date as Date)
-        print(dateString)
-        //Custom Date Format Sample 2 28-Feb-2016 11:41:51
+    func prover(date1:NSDate,date2:NSDate)->Bool{
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .short
+        var dateString1 = dateFormatter1.string(from: date1 as Date)
+        
+        let dateFormatter2 = DateFormatter()
+        dateFormatter2.dateStyle = .short
+        var dateString2 = dateFormatter2.string(from: date2 as Date)
+        
+        if(dateString1 == dateString2){
+            dateFormatter1.dateFormat = "hh"
+            dateString1 = dateFormatter1.string(from: date1 as Date)
+            var mm1:Int=Int(dateString1)!*60
+            dateFormatter1.dateFormat = "mm"
+            dateString1 = dateFormatter1.string(from: date1 as Date)
+            mm1=mm1+Int(dateString1)!
+            dateFormatter2.dateFormat = "hh"
+            dateString2 = dateFormatter2.string(from: date2 as Date)
+            var mm2:Int=Int(dateString2)!*60
+            dateFormatter2.dateFormat = "mm"
+            dateString2 = dateFormatter2.string(from: date2 as Date)
+            mm2=mm2+Int(dateString2)!
+            if(mm2-mm1>5){return true}
+            else {return false}
+        }else{return true}
+        
     }
     
     func safe( str1:inout String,str2:inout String){
@@ -71,11 +111,22 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
             request(url).responseJSON{
                 respons2 in
                 let swiftyJsonVar2 = JSON(respons2.result.value)
-                let temp = swiftyJsonVar2["main","temp"]
+                let temp = swiftyJsonVar2["main","temp"].rawString()!
                 print(temp)
-                let icon = swiftyJsonVar2["weather",0,"icon"]
+                let icon = swiftyJsonVar2["weather",0,"icon"].rawString()!
                 print (icon)
-            
+                var inform1 = inform()
+                print("sucsess")
+                let results = self.realm.objects(inform.self)
+                try! self.realm.write {
+                    inform1.city = self.city
+                    inform1.date = NSDate()
+                    inform1.imageName=icon
+                    inform1.temp = temp
+                    inform1.id=results.count
+                    self.realm.add(inform1)
+                }
+            print("sucsess")
             }
         }
     }
